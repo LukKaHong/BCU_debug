d4317427394aafcadf3ad623248a8591b0daa16a
jiakang.lu <lukkahong@qq.com>
2025-12-11 14:54:38 +0800

    1

--- User/App/Comm485.c
+++ User/App/Comm485.c
@@ -30,92 +30,97 @@ static void Comm_485_Read_Pro(uint8_t port, uint8_t *tx_buff, uint8_t *rx_buff)
 
     for(uint8_t device_num = 0; device_num < modbus->device_num; device_num++)//扫描所有设备
     {
-
-        if(modbus->device_attr[device_num].protocol != PROTOCOL_MODBUS)
-            continue;
-
-        //获取协议
-        ProtocolConvert_modbus_t* convert = GetProtocolConvert_modbus(modbus->device_attr[device_num].device_type);
-        if(convert == NULL)
-            continue;
-        
-        for(uint8_t area_num = 0; area_num < convert->area_num; area_num++)//扫描所有区域
+        if(modbus->device_attr[device_num].protocol == PROTOCOL_MODBUS)
         {
-            if(++modbus->device_attr[device_num].cyclecnt[area_num] >= convert->area_attr[area_num].cycle / Comm485_Task_Cycle)//定时查询
+            //获取协议
+            ProtocolConvert_modbus_t* convert = GetProtocolConvert_modbus(modbus->device_attr[device_num].device_type);
+            if(convert == NULL)
+                continue;
+        
+            for(uint8_t area_num = 0; area_num < convert->area_num; area_num++)//扫描所有区域
             {
-                modbus->device_attr[device_num].cyclecnt[area_num] = 0;
-
-                uint16_t tx_len = 0;
-                const uint8_t* data;
-                uint16_t data_len;
-                int32_t result = MB_ERR_ARG;
-
-                if(convert->area_attr[area_num].fun_code == MB_FC_READ_COILS)
+                if(++modbus->device_attr[device_num].cyclecnt[area_num] >= convert->area_attr[area_num].cycle / Comm485_Task_Cycle)//定时查询
                 {
-                    ModbusRTU_BuildReadCoils(
-                        modbus->device_attr[device_num].device_addr,//设备地址
-                        convert->area_attr[area_num].reg_addr,//寄存器地址
-                        convert->area_attr[area_num].reg_num,//寄存器数量
-                        tx_buff,
-                        &tx_len
-                    );
-                    
-                    result = ModbusRTU_ParseReadCoilsRsp(
-                            rx_buff,
-                            _485_Tx_And_Rx(port, tx_buff, tx_len, rx_buff, Uart_Rx_Buff_Size),
-                            modbus->device_attr[device_num].device_addr,//设备地址
-                            convert->area_attr[area_num].reg_num,//寄存器数量
-                            &data,//存寄存器数据的指针
-                            &data_len//字节数
-                        );
+                    modbus->device_attr[device_num].cyclecnt[area_num] = 0;
 
-                }
-                else if(convert->area_attr[area_num].fun_code == MB_FC_READ_HOLDING_REGS)
-                {
-                    ModbusRTU_BuildReadHolding(
-                        modbus->device_attr[device_num].device_addr,//设备地址
-                        convert->area_attr[area_num].reg_addr,//寄存器地址
-                        convert->area_attr[area_num].reg_num,//寄存器数量
-                        tx_buff,
-                        &tx_len
-                    );
+                    uint16_t tx_len = 0;
+                    const uint8_t* data;
+                    uint16_t data_len;
+                    int32_t result = MB_ERR_ARG;
 
-                    result = ModbusRTU_ParseReadHoldingRsp(
-                            rx_buff,
-                            _485_Tx_And_Rx(port, tx_buff, tx_len, rx_buff, Uart_Rx_Buff_Size),
-                            modbus->device_attr[device_num].device_addr,//设备地址
-                            convert->area_attr[area_num].reg_num,//寄存器数量
-                            &data,//存寄存器数据的指针
-                            &data_len//字节数
-                        );
-                }
-
-                if(result == MB_OK)
-                {
-                    uint16_t reg_addr = convert->area_attr[area_num].reg_addr;
-
-                    for(uint16_t reg_num = 0; reg_num < convert->area_attr[area_num].reg_num; reg_num++)//扫描读取出来的寄存器
+                    if(convert->area_attr[area_num].fun_code == MB_FC_READ_COILS)
                     {
-                        for(uint16_t node_num = 0; node_num < convert->node_num; node_num++)//扫描所有点表
+                        ModbusRTU_BuildReadCoils(
+                            modbus->device_attr[device_num].device_addr,//设备地址
+                            convert->area_attr[area_num].reg_addr,//寄存器地址
+                            convert->area_attr[area_num].reg_num,//寄存器数量
+                            tx_buff,
+                            &tx_len
+                        );
+                        
+                        result = ModbusRTU_ParseReadCoilsRsp(
+                                rx_buff,
+                                _485_Tx_And_Rx(port, tx_buff, tx_len, rx_buff, Uart_Rx_Buff_Size),
+                                modbus->device_attr[device_num].device_addr,//设备地址
+                                convert->area_attr[area_num].reg_num,//寄存器数量
+                                &data,//存寄存器数据的指针
+                                &data_len//字节数
+                            );
+
+                    }
+                    else if(convert->area_attr[area_num].fun_code == MB_FC_READ_HOLDING_REGS)
+                    {
+                        ModbusRTU_BuildReadHolding(
+                            modbus->device_attr[device_num].device_addr,//设备地址
+                            convert->area_attr[area_num].reg_addr,//寄存器地址
+                            convert->area_attr[area_num].reg_num,//寄存器数量
+                            tx_buff,
+                            &tx_len
+                        );
+
+                        result = ModbusRTU_ParseReadHoldingRsp(
+                                rx_buff,
+                                _485_Tx_And_Rx(port, tx_buff, tx_len, rx_buff, Uart_Rx_Buff_Size),
+                                modbus->device_attr[device_num].device_addr,//设备地址
+                                convert->area_attr[area_num].reg_num,//寄存器数量
+                                &data,//存寄存器数据的指针
+                                &data_len//字节数
+                            );
+                    }
+
+                    if(result == MB_OK)
+                    {
+                        uint16_t reg_addr = convert->area_attr[area_num].reg_addr;
+
+                        for(uint16_t reg_num = 0; reg_num < convert->area_attr[area_num].reg_num; reg_num++)//扫描读取出来的寄存器
                         {
-                            if(convert->node_attr[node_num].reg_addr == reg_addr && 
-                                convert->node_attr[node_num].fun_code == convert->area_attr[area_num].fun_code)//匹配点表
+                            for(uint16_t node_num = 0; node_num < convert->node_num; node_num++)//扫描所有点表
                             {
-                                uint16_t index = 0;
-
-                                if(ModelIdToNodeIndex(modbus->device_attr[device_num].device_type, modbus->device_attr[device_num].device_no, convert->node_attr[node_num].model_id, &index) == true)
+                                if(convert->node_attr[node_num].reg_addr == reg_addr && 
+                                    convert->node_attr[node_num].fun_code == convert->area_attr[area_num].fun_code)//匹配点表
                                 {
-                                    ConvertToNode_modbus(GetNodePointer() + index, (uint8_t*)data, &convert->node_attr[node_num]);
-                                }
+                                    uint16_t index = 0;
 
-                                break;
+                                    if(ModelIdToNodeIndex(modbus->device_attr[device_num].device_type, modbus->device_attr[device_num].device_no, convert->node_attr[node_num].model_id, &index) == true)
+                                    {
+                                        ConvertToNode_modbus(GetNodePointer() + index, (uint8_t*)data, &convert->node_attr[node_num]);
+                                    }
+
+                                    break;
+                                }
                             }
-                        }
-                        data += 2;//指针增加2字节
-                        reg_addr++;//寄存器地址增加1
-                    } 
+                            data += 2;//指针增加2字节
+                            reg_addr++;//寄存器地址增加1
+                        } 
+                    }
                 }
             }
+
+        }
+        else
+        {
+
+
         }
     }
 }
@@ -132,38 +137,46 @@ static void Comm_485_Write_Pro(uint8_t port, uint8_t *tx_buff, uint8_t *rx_buff)
 
     for(uint8_t device_num = 0; device_num < modbus->device_num; device_num++)//扫描所有设备
     {
-        uint16_t start = 0, end = 0;
-        if(GetNodeRange(modbus->device_attr[device_num].device_type, modbus->device_attr[device_num].device_no, &start, &end) == false)
-            continue;
-
-        Write_Node_t* write_node = GetWriteNodePointer();
-
-        for(uint16_t index = start; index <= end; index++)//扫描所有点表
+        if(modbus->device_attr[device_num].protocol == PROTOCOL_MODBUS)
         {
-            if(write_node->writeflag[index] == 1)//判断是否需要写入
+            uint16_t start = 0, end = 0;
+            if(GetNodeRange(modbus->device_attr[device_num].device_type, modbus->device_attr[device_num].device_no, &start, &end) == false)
+                continue;
+
+            Write_Node_t* write_node = GetWriteNodePointer();
+
+            for(uint16_t index = start; index <= end; index++)//扫描所有点表
             {
-                write_node->writeflag[index] = 0;
-
-                uint16_t model_id = 0;
-                if(NodeIndexToModelId(modbus->device_attr[device_num].device_type, modbus->device_attr[device_num].device_no, index, &model_id) == false)
-                    continue;
-
-                uint16_t reg_addr = 0;
-                uint8_t fun_code = 0xff;
-                if(ModelIdToRegAddr_modbus(modbus->device_attr[device_num].device_type, model_id, &reg_addr, &fun_code) == false)
-                    continue;
-
-                uint16_t tx_len = 0;
-                uint16_t rx_len = 0;
-                uint16_t value = 0;
-
-                if(fun_code == MB_FC_READ_HOLDING_REGS)
+                if(write_node->writeflag[index] == 1)//判断是否需要写入
                 {
-                    ModbusRTU_BuildWriteSingle(modbus->device_attr[device_num].device_addr, reg_addr, write_node->value[index], tx_buff, &tx_len);
-                    rx_len = _485_Tx_And_Rx(port, tx_buff, tx_len, rx_buff, Uart_Rx_Buff_Size);
-                    ModbusRTU_ParseWriteSingleRsp(rx_buff, rx_len, modbus->device_attr[device_num].device_addr, reg_addr, &value);
+                    write_node->writeflag[index] = 0;
+
+                    uint16_t model_id = 0;
+                    if(NodeIndexToModelId(modbus->device_attr[device_num].device_type, modbus->device_attr[device_num].device_no, index, &model_id) == false)
+                        continue;
+
+                    uint16_t reg_addr = 0;
+                    uint8_t fun_code = 0xff;
+                    if(ModelIdToRegAddr_modbus(modbus->device_attr[device_num].device_type, model_id, &reg_addr, &fun_code) == false)
+                        continue;
+
+                    uint16_t tx_len = 0;
+                    uint16_t rx_len = 0;
+                    uint16_t value = 0;
+
+                    if(fun_code == MB_FC_READ_HOLDING_REGS)
+                    {
+                        ModbusRTU_BuildWriteSingle(modbus->device_attr[device_num].device_addr, reg_addr, write_node->value[index], tx_buff, &tx_len);
+                        rx_len = _485_Tx_And_Rx(port, tx_buff, tx_len, rx_buff, Uart_Rx_Buff_Size);
+                        ModbusRTU_ParseWriteSingleRsp(rx_buff, rx_len, modbus->device_attr[device_num].device_addr, reg_addr, &value);
+                    }
                 }
             }
+        }
+        else
+        {
+
+            
         }
     }
 }
